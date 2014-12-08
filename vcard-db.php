13a25a01a85ca3ed6a15b7c9c84a9d17468a5bca
Eric Vought <evought@pobox.com>
2014-12-08 16:05:31 -0600

    Corrected bad case in namespace declaration from earlier commit.

--- vcard-db.php
+++ vcard-db.php
@@ -5,7 +5,7 @@
  * @copyright Eric Vought 2014, Some rights reserved.
  * @license MIT http://opensource.org/licenses/MIT
  */
-namespace Evought\vCardTools;
+namespace EVought\vCardTools;
 
 
 require_once "VCard.php";
