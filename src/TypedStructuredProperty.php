3761cece2f8e50d4e89a21cf6f38c3316993d276
Eric Vought <evought@pobox.com>
2014-12-17 01:50:50 -0600

    Added a typed and structured property.

---
+++ src/TypedStructuredProperty.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * A Property which is both Typed and Structured.
+ * @author Eric Vought <evought@pobox.com>
+ * @copyright Eric Vought 2014, Some rights reserved.
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 evought.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace EVought\vCardTools;
+
+/**
+ * A Property which is both Typed and Structured,
+ *
+ * @author evought
+ */
+class TypedStructuredProperty implements TypedProperty, StructuredProperty
+{
+    use TypedPropertyTrait, StructuredPropertyTrait;
+    
+    public function __construct(TypedStructuredPropertyBuilder $builder)
+    {
+        $this->setNameFromBuilder($builder);
+        $this->setValueFromBuilder($builder);
+        \assert(is_array($this->value));
+        $this->setTypesFromBuilder($builder);
+    }
+}
---
+++ tests/TypedStructuredPropertyBuilderTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 evought.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace EVought\vCardTools;
+
+/**
+ * Description of TypedStructuredPropertyBuilderTest
+ *
+ * @author evought
+ */
+class TypedStructuredPropertyBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $builder = new TypedStructuredPropertyBuilder(
+            'adr', ['work', 'home'], ['Locality', 'Region'] );
+        $this->assertInstanceOf( 'EVought\vCardTools\TypedPropertyBuilder',
+            $builder );
+        $this->assertInstanceOf( 'EVought\vCardTools\StructuredPropertyBuilder',
+            $builder );
+    }
+    
+    /**
+     * @depends testConstruct
+     */
+    public function testSetAndBuild()
+    {
+        $builder = new TypedStructuredPropertyBuilder(
+            'adr', ['work', 'home'], ['Locality', 'Region'] );
+        $builder->setValue(['Locality'=>'Manhattan', 'Region'=>'Kansas'])
+                ->setTypes(['work']);
+        
+        $property = $builder->build();
+        
+        $this->assertInstanceOf('EVought\vCardTools\TypedProperty', $property);
+        $this->assertInstanceOf( 'EVought\vCardTools\StructuredProperty',
+                                    $property );
+        
+        $this->assertEquals('adr', $property->getName());
+        $this->assertEquals( ['Locality'=>'Manhattan', 'Region'=>'Kansas'],
+                                $property->getValue() );
+        $this->assertEquals(['work'], $property->getTypes());
+    }
+}
